21c15463bd62518842e8ee5fccf996e619407967
Dominic DeRosa <dderosa71@gmail.com>
2021-08-13 18:22:42 +0200

    Done.

--- src/components/SpicyFoodList.js
+++ src/components/SpicyFoodList.js
@@ -4,15 +4,33 @@ import { spicyFoods, getNewSpicyFood } from "../data";
 function SpicyFoodList() {
   const [foods, setFoods] = useState(spicyFoods);
 
+
   function handleAddFood() {
     const newFood = getNewSpicyFood();
     console.log(newFood);
+    setFoods((foods)=> [...foods, newFood]) 
   }
+
+  function handleClick(id){
+    const filteredFoods = foods.map((food)=> {
+      if(food.id !== id){
+        return food
+      }else{
+      return {...food, heatLevel: food.heatLevel+1}
+      }})
+    setFoods(filteredFoods)
+  }
+
+  const foodList = foods.map((food) => (
+    <li key={food.id} onClick={()=>handleClick(food.id)}>
+        {food.name} | Heat: {food.heatLevel} | Cuisine: {food.cuisine}
+        </li>
+  ))
 
   return (
     <div>
       <button onClick={handleAddFood}>Add New Food</button>
-      <ul>{/* list of spicy foods */}</ul>
+      <ul>{foodList}</ul>
     </div>
   );
 }
